8ab5b70020e48a5dade085cf2d6a7a803d4f7562
arjun singh tomar <arjunsinghtomar03511@gmail.com>
2025-02-18 22:47:16 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,14 @@
+export default function CustomersPage() {
+    return (
+        <div>
+            <h1>Customers Page</h1>
+            <p>List of customers will be displayed here.</p>
+            <ul>
+                {/* Example customer list items can go here */}
+                <li>Customer 1</li>
+                <li>Customer 2</li>
+                <li>Customer 3</li>
+            </ul>
+        </div>
+    );
+}
